9ae19f5f93c8b9225685b0742859239660512d81
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2022-12-04 10:47:46 +0000

    Adding functionality for adding a contact

--- controllers/phonebook-controller.js
+++ controllers/phonebook-controller.js
@@ -2,10 +2,11 @@ const phonebook = require('../phonebook');
 
 module.exports = {
   index: (req, res) => {
-    res.render('index');
-    // TODO: load index page
+    let contacts = phonebook.getData();
+    res.render('index', { contacts });
   },
-  addPhonebookPost:(req, res) => {
-    // TODO: add a phonebook object to the array
+  addPhonebookPost: (req, res) => {
+    phonebook.addContact(req.body.name, req.body.number);
+    res.redirect('/');
   }
 }
--- phonebook.js
+++ phonebook.js
@@ -1,5 +1,18 @@
-/* TODO: 
-	create phonebook array
-	add methods for adding in the phonebook and getting it
-	export the methods
-*/
+let fs = require('fs');
+const Contact = require("./models/Contact");
+
+let phonebook = JSON.parse(fs.readFileSync('./data.json'));
+function getData() {
+	return phonebook;
+}
+
+function addContact(name, number) {
+	let contacts = { name, number };
+	phonebook.push(contacts);
+	fs.writeFileSync('./data.json', JSON.stringify(phonebook));
+}
+
+module.exports = {
+	getData,
+	addContact
+}
